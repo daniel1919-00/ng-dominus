f4f22c5c2930767a7e7af78532a7a45a213d7fff
Daniel <45707084+daniel1919-00@users.noreply.github.com>
2024-01-05 11:54:25 +0200

    [dm-table] init

--- projects/dm-table/src/lib/dm-table.component.ts
+++ projects/dm-table/src/lib/dm-table.component.ts
@@ -1,13 +1,330 @@
-import {Component} from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Inject,
+    Input,
+    OnChanges,
+    OnDestroy,
+    Optional,
+    Output,
+    SimpleChanges,
+    ViewChild
+} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {MatSort, MatSortModule} from "@angular/material/sort";
+import {
+    DmTableColumnDefinition, DmTableColumnVisibility,
+    DmTableDataSource,
+    DmTableDataSourceAdapter,
+    DmTableRequestOptions,
+    DOMINUS_TABLE_INTL,
+    DominusTableIntl
+} from "./dm-table";
+import {Subject, Subscription} from "rxjs";
+import {MatMenu, MatMenuModule} from "@angular/material/menu";
+import {FormGroup, UntypedFormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {SelectionModel} from "@angular/cdk/collections";
+import {MatCheckboxChange, MatCheckboxModule} from "@angular/material/checkbox";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @Component({
     selector: 'dm-table',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule, MatPaginatorModule, MatSortModule, MatMenuModule, MatCheckboxModule, MatProgressSpinnerModule],
     templateUrl: './dm-table.component.html',
-    styleUrl: './dm-table.component.scss'
+    styleUrl: './dm-table.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DmTableComponent {
+export class DmTableComponent implements OnChanges, AfterViewInit, OnDestroy {
+    /**
+     * Table column definitions.
+     */
+    @Input({required: true}) columns!: DmTableColumnDefinition[];
+    /**
+     * The data source from which rows are fetched.
+     * Can be a static array of rows or an url to a server side source.
+     * Note: When using server side data source, it is assumed that sorting and pagination is also handled on the server.
+     */
+    @Input({required: true}) dataSource!: DmTableDataSource;
+    /**
+     * The request method used when calling the server side data source(GET, POST, etc.).
+     */
+    @Input() dataSourceRequestMethod: string = 'GET';
+    /**
+     * Filters that will be sent to the server side data source along with the default parameters (paging, sorting, etc.).
+     */
+    @Input() filters?: UntypedFormGroup | FormGroup;
+    /**
+     * A function that takes the current request options and returns a promise which resolves the final request options to be passed to the angular HttpClient.
+     * This is useful if you need to pass additional headers (e.g. Authorization) or modify the data before sending the request to the backend.
+     * The same effect can be achieved using an interceptor.
+     */
+    @Input() onBeforeServerRequestFn?: (requestOptions: DmTableRequestOptions) => Promise<DmTableRequestOptions>;
+    /**
+     * Adds a contextual menu on each row in a special column added at the end of the table.
+     * Use the [rowContextMenuDisplayCondition] @Input to control when this menu is displayed.
+     */
+    @Input() rowContextMenu: MatMenu | null = null;
+    /**
+     * Function which determines when the contextual menu is displayed
+     */
+    @Input() rowContextMenuIsVisibleFn: (row: any) => boolean = () => true;
+    /**
+     * If set, a column with checkboxes for each row will be appended at the beginning of the table.
+     * The SelectionModel will be populated with each of the selected row's data.
+     * Note: The table respects whether the selection model supports multiple values or not
+     */
+    @Input() rowSelectionModel?: SelectionModel<any>;
+    /**
+     * A function that takes the rows as an argument and returns them back.
+     * This is useful when you need to alter row data from a server side data source before being rendered.
+     */
+    @Input() mapRowsFn?: (rows: any[]) => any[];
+    /**
+     * Weather a highlight effect will be rendered when the user hovers the cursor over a row.
+     */
+    @Input() rowHoverEffectEnabled = false;
+    /**
+     * Whether to display a loading spinner when loading data.
+     * This takes effect only when using a server-side data sources.
+     */
+    @Input() loadingDataOverlay = true;
+    /**
+     * Whether pagination will be enabled.
+     */
+    @Input() paginate = true;
+    /**
+     * The default page size.
+     */
+    @Input() pageSize = 10;
+    /**
+     * The page sizes available to the user.
+     */
+    @Input() pageSizeOptions = [5, 10, 20, 30, 40, 50];
+    /**
+     * The position of the sorting arrow
+     */
+    @Input() sortingArrowPosition: 'before' | 'after' = 'after';
 
+    /**
+     * Event triggered when a row has been clicked.
+     * The event contains the row data.
+     */
+    @Output() rowClicked = new Subject<any>();
+
+    protected displayedColumns: string[] = [];
+    protected dataSourceAdapter!: DmTableDataSourceAdapter;
+    protected isLoadingData = false;
+    protected loadingDataSub?: Subscription;
+    protected masterCheckboxChecked = false;
+    protected masterCheckboxIndeterminate = false;
+    protected readonly intl: Record<DominusTableIntl, string>;
+    protected readonly DominusTableIntl = DominusTableIntl;
+
+    @ViewChild(MatPaginator) private paginator?: MatPaginator;
+    @ViewChild(MatSort) private sort!: MatSort;
+
+    constructor(
+        private changeDetector: ChangeDetectorRef,
+        private http: HttpClient,
+        @Optional() @Inject(DOMINUS_TABLE_INTL) intl?: Record<DominusTableIntl, string>,
+    ) {
+        this.intl = Object.assign({
+            [DominusTableIntl.NO_DATA]: 'No data'
+        }, intl || {});
+    }
+
+    ngAfterViewInit() {
+        this.setDataSrcAdapter(true);
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        const headerNeedsUpdate = changes['columns'] || (changes['rowSelectionModel'] && !changes['rowSelectionModel'].firstChange);
+
+        if (
+            headerNeedsUpdate
+            || (changes['rowHoverEffectEnabled'] && !changes['rowHoverEffectEnabled'].firstChange)
+            || (changes['pageSizeOptions'] && !changes['pageSizeOptions'].firstChange)
+            || (changes['paginate'] && !changes['paginate'].firstChange)
+            || (changes['loadingDataOverlay'] && !changes['loadingDataOverlay'].firstChange)
+            || (changes['sortingArrowPosition'] && !changes['sortingArrowPosition'].firstChange)
+            || (changes['rowContextMenu'] && !changes['rowContextMenu'].firstChange)
+        ) {
+            headerNeedsUpdate && this.prepareDisplayedColumns();
+            this.changeDetector.markForCheck();
+        }
+
+        if (changes['dataSource'] && !changes['dataSource'].firstChange) {
+            this.setDataSrcAdapter();
+        }
+    }
+
+    /**
+     * Refreshes the table data
+     * @param resetPage whether the page will be reset as well
+     */
+    refresh(resetPage = true) {
+        this.dataSourceAdapter.refresh(resetPage);
+    }
+
+    /**
+     * Hides/shows or toggles the specified table column's visibility
+     * @param columnId
+     * @param visible If not specified, the visibility will be toggled
+     */
+    changeColumnVisibility(columnId: string, visible?: boolean) {
+        const column = this.getColumn(columnId);
+        if(!column) {
+            return;
+        }
+
+        column.visible = typeof visible === 'undefined' ? !column.visible : visible;
+        this.prepareDisplayedColumns();
+        this.changeDetector.markForCheck();
+    }
+
+    /**
+     * Changes the visibility of multiple columns
+     * @param columns The columns and their visibility. Example: [{columnId: 'myColumn1', visible: true}, {columnId: 'myOtherColumn', visible: false}] -- sets myColumn1 to visible and hides myOtherColumn
+     */
+    changeColumnsVisibility(columns: DmTableColumnVisibility[]) {
+        for(let i = columns.length; i--;) {
+            const colVisibilityConfig = columns[i];
+            const column = this.getColumn(colVisibilityConfig.columnId);
+            if(!column) {
+                continue;
+            }
+
+            column.visible = typeof colVisibilityConfig.visible !== "undefined" ? colVisibilityConfig.visible : !column.visible;
+        }
+
+        this.prepareDisplayedColumns();
+        this.changeDetector.markForCheck();
+    }
+
+    protected toggleAllRowsSelection($event: MatCheckboxChange) {
+        const rowSelectionModel = this.rowSelectionModel;
+        if(!rowSelectionModel) {
+            return;
+        }
+
+        const rows = this.dataSourceAdapter.data;
+        if($event.checked) {
+            for(let i = rows.length; i--;) {
+                rowSelectionModel.select(rows[i]);
+            }
+
+            this.masterCheckboxChecked = true;
+            this.masterCheckboxIndeterminate = rowSelectionModel.selected.length !== this.dataSourceAdapter.getTotalResults();
+        } else {
+            rowSelectionModel.clear();
+            this.masterCheckboxChecked = false;
+            this.masterCheckboxIndeterminate = false;
+        }
+    }
+
+    protected toggleRowSelection(row: any) {
+        const rowSelectionModel = this.rowSelectionModel;
+        if(!rowSelectionModel) {
+            return;
+        }
+
+        rowSelectionModel.toggle(row);
+
+        if(rowSelectionModel.isEmpty()) {
+            this.masterCheckboxChecked = false;
+            this.masterCheckboxIndeterminate = false;
+        } else {
+            this.masterCheckboxChecked = true;
+            this.masterCheckboxIndeterminate = rowSelectionModel.selected.length !== this.dataSourceAdapter.getTotalResults();
+        }
+    }
+
+    private getColumn(columnId: string) {
+        const columns = this.columns;
+        let foundColumn: undefined|DmTableColumnDefinition;
+
+        for(let i = columns.length; i--;) {
+            const col = columns[i];
+            if(col.id === columnId) {
+                foundColumn = col;
+                break;
+            }
+        }
+
+        return foundColumn;
+    }
+
+    private prepareDisplayedColumns() {
+        const columns = this.columns;
+        const colLen = columns.length;
+        const _displayedColumns = [];
+
+        if(this.rowSelectionModel) {
+            _displayedColumns.push('dm_table_row_selection_column');
+        }
+
+        for (let colIndex = 0; colIndex < colLen; ++colIndex) {
+            const column = columns[colIndex];
+            if (column.visible === undefined || column.visible) {
+                _displayedColumns.push(column.id);
+            }
+        }
+
+        if (this.rowContextMenu) {
+            _displayedColumns.push('dm_table_row_contextual_menu_column');
+        }
+
+        this.displayedColumns = _displayedColumns;
+    }
+
+    private setDataSrcAdapter(initialSet = false) {
+        if(!initialSet) {
+            this.loadingDataSub?.unsubscribe();
+            if(this.sort.active !== '') {
+                this.sort.active = '';
+                this.sort.direction = '';
+                this.sort._stateChanges.next();
+            }
+
+            if(this.paginator) {
+                this.paginator.length = 0;
+                this.paginator.pageIndex = 0;
+            }
+        }
+
+        const dataSourceAdapter = new DmTableDataSourceAdapter(
+            this.dataSourceRequestMethod,
+            this.http,
+            this.dataSource,
+            this.sort,
+            this.onBeforeServerRequestFn || ((requestOptions: DmTableRequestOptions) => new Promise((resolve) => { resolve(requestOptions); })),
+            this.paginator,
+            this.mapRowsFn,
+            this.filters
+        );
+
+        if(this.loadingDataOverlay) {
+            this.loadingDataSub = dataSourceAdapter.onDataLoading().subscribe((loading) => {
+                if(this.isLoadingData !== loading) {
+                    this.isLoadingData = loading;
+                    this.changeDetector.detectChanges();
+                }
+            });
+        }
+
+        this.dataSourceAdapter = dataSourceAdapter;
+    }
+
+    ngOnDestroy() {
+        this.rowClicked.complete();
+        this.loadingDataSub?.unsubscribe();
+    }
 }
---
+++ projects/dm-table/src/lib/dm-table.ts
@@ -0,0 +1,182 @@
+import {BehaviorSubject, merge, Observable, Subscription} from "rxjs";
+import {HttpClient, HttpContext, HttpHeaders, HttpParams} from "@angular/common/http";
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {FormGroup, UntypedFormGroup} from "@angular/forms";
+import {InjectionToken} from "@angular/core";
+import {MatTableDataSource} from "@angular/material/table";
+
+export interface DmTableColumnDefinition {
+    /**
+     * This will be matched against the data source when providing value for this column.
+     * For example the column id 'my_column' should be found in the data source, like so: [{'my_column': 'column_value'}, ...].
+     */
+    id: string;
+    /**
+     * The column display name.
+     */
+    name: string;
+    /**
+     * Classes to be placed on the whole column (th & td).
+     * The values must be compatible with ngClass.
+     */
+    classes?: string[] | { [className: string]: boolean };
+    /**
+     * Is this column sortable?
+     */
+    sortable?: boolean;
+    /**
+     * Whether this column is displayed or hidden.
+     */
+    visible?: boolean;
+}
+
+export interface DmTableRow {
+    [columnId: string]: any;
+}
+
+export type DmTableDataSource = string | { [columnId: string]: any }[];
+
+export interface DmTableDataServerResponse {
+    totalResults: number;
+    rows: DmTableRow[];
+}
+
+export interface DmTableRequestOptions {
+    body?: any;
+    headers?: HttpHeaders | {
+        [header: string]: string | string[];
+    };
+    context?: HttpContext;
+    observe?: 'body';
+    params?: HttpParams | {
+        [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
+    };
+    responseType?: 'json';
+}
+
+export interface DmTableColumnVisibility {
+    columnId: string;
+    /**
+     * If not specified, the visibility will be toggled
+     */
+    visible?: boolean;
+}
+
+export enum DominusTableIntl {
+    NO_DATA
+}
+
+export const DOMINUS_TABLE_INTL = new InjectionToken<Record<DominusTableIntl, string>>('Dominus table i18n strings');
+
+export class DmTableDataSourceAdapter extends MatTableDataSource<any> {
+    private totalResults = 0;
+    private eventsSub: Subscription;
+    private loadingData$ = new BehaviorSubject<boolean>(false);
+    private readonly paginatorRef?: MatPaginator;
+    private readonly sortRef?: MatSort;
+
+    constructor(
+        private requestMethod: string,
+        private http: HttpClient,
+        private dataSrc: DmTableDataSource,
+        sort: MatSort,
+        private readonly onBeforeRequest: (requestOptions: DmTableRequestOptions) => Promise<DmTableRequestOptions>,
+        paginator?: MatPaginator,
+        private mapRowsFn?: (rows: any[]) => any[],
+        private filters?: UntypedFormGroup | FormGroup,
+    ) {
+        super();
+
+        this.sortRef = sort;
+        this.paginatorRef = paginator;
+
+        if(typeof dataSrc !== 'string') {
+            this.sort = sort;
+            if(paginator) {
+                this.paginator = paginator;
+            }
+        }
+
+        this.eventsSub = (paginator?.page ? merge(sort.sortChange, paginator.page) : sort.sortChange).subscribe((event: any) => this.refresh(event.pageIndex === undefined));
+    }
+
+    override connect() {
+        this.refresh(false);
+        return super.connect();
+    }
+
+    getTotalResults() {
+        return this.totalResults;
+    }
+
+    refresh(resetPage = true) {
+        if(this.loadingData$.getValue()) {
+            return;
+        }
+
+        const paginator = this.paginatorRef;
+        const dataSrc = this.dataSrc;
+
+        if (resetPage && paginator) {
+            paginator.pageIndex = 0;
+        }
+
+        if (typeof dataSrc === 'string') {
+            return this.handleServerSideDataSrc(dataSrc);
+        }
+
+        const mapRowsFn = this.mapRowsFn;
+        this.data = mapRowsFn ? mapRowsFn(dataSrc) : dataSrc;
+        this.totalResults = this.data.length;
+    }
+
+    onDataLoading(): Observable<boolean> {
+        return this.loadingData$.asObservable();
+    }
+
+    private handleServerSideDataSrc(dataSource: string) {
+        this.loadingData$.next(true);
+
+        const requestMethod = this.requestMethod.toUpperCase();
+        const requestOptions: DmTableRequestOptions = {
+            responseType: 'json',
+            observe: "body"
+        };
+
+        const requestData = this.filters?.value || {};
+        requestData['dm_sort_col'] = this.sortRef?.active || '';
+        requestData['dm_sort_dir'] = this.sortRef?.direction || '';
+        requestData['dm_page_index'] = this.paginatorRef?.pageIndex || 0;
+        requestData['dm_page_len'] = this.paginatorRef?.pageSize || 0;
+
+        if(requestMethod !== 'GET')
+        {
+            requestOptions.params = requestData;
+        }
+        else
+        {
+            requestOptions.body = requestData;
+        }
+
+        this.onBeforeRequest(requestOptions).then(options => {
+            this.http.request<DmTableDataServerResponse>(requestMethod, dataSource, options).subscribe((response) => {
+                if (response) {
+                    if (this.paginatorRef) {
+                        this.paginatorRef.length = response.totalResults;
+                    }
+                    this.totalResults = response.totalResults;
+
+                    this.data = this.mapRowsFn ? this.mapRowsFn(response.rows) : response.rows;
+                }
+                this.loadingData$.next(false);
+            });
+        });
+    }
+
+    override disconnect(): void {
+        this.eventsSub.unsubscribe();
+        this.loadingData$.complete();
+        super.disconnect();
+    }
+}
