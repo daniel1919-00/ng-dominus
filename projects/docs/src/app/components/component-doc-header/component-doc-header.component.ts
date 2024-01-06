f2b1f8070701a57ee2e07d0659131f9c382da5d3
Daniel <45707084+daniel1919-00@users.noreply.github.com>
2024-01-06 13:26:44 +0200

    [docs] Added npm repo link to all components

--- projects/docs/src/app/components/component-doc-header/component-doc-header.component.ts
+++ projects/docs/src/app/components/component-doc-header/component-doc-header.component.ts
@@ -1,14 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
     selector: 'app-component-doc-header',
     standalone: true,
-    imports: [CommonModule, MatCardModule],
+    imports: [CommonModule, MatCardModule, MatIconModule],
     templateUrl: './component-doc-header.component.html',
     styleUrl: './component-doc-header.component.scss'
 })
 export class ComponentDocHeaderComponent {
     @Input({required: true}) title!: string;
+    @Input() titleLink: string|null = null;
 }
